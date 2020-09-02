57597352c992f33fc208d0c8dc9680381c2855bf
Trevor Guinn <tdguinn87@gmail.com>
2020-09-01 20:17:06 -0500

    Updated main.js
    completed brick breaker program

--- js/main.js
+++ js/main.js
@@ -73,4 +73,110 @@ function keyUpHandler(e) {
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x,y,ballRadius,0,Math.PI*2); //centered at (x,y) position with radius r = ballRadius starting at 0 = startAngle, ending at Math.PI*2 = endAngle (in Radians)
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    ctx.closePath();
 }
+
+//Create a function to create the paddle
+function drawPaddle(){
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight,paddleWidth,paddleHeight); //centered at (x,y) position with radius r = ballRadius starting at 0 = startAngle, ending at Math.PI*2 = endAngle (in Radians)
+    ctx.fillStyle = 'blue';
+    ctx.fill();
+    ctx.closePath();
+}
+
+//create a function to draw the bricks
+function drawBricks(){
+    for(c = 0; c < brickColumnCount; c++){
+        for(r = 0; r < brickRowCount; r++){
+            if(bricks[c][r].status === 1){
+                let brickX = (c* (brickWidth + brickPadding)) + brickOffsetLeft;
+                let brickY = (r* (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = '#6600cc';
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+//Create function to keep track of score
+function drawScore(){
+    ctx.font = '18px Arial';
+    ctx.fillStyle = 'brown';
+    ctx.fillText('score: ' + score, 8, 20); //position score at 8, 20 on the x,y axis of the canvas
+}
+
+//Collision detection for the bricks
+function collisionDetection(){
+    for(c = 0; c < brickColumnCount; c++){
+        for(r = 0; r < brickRowCount; r++){
+            let b = bricks[c][r];
+            if(b.status === 1){
+                if(x > b.x && x <b.x + brickWidth && y > b.y && y < b.y + brickHeight){
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if(score === brickRowCount * brickColumnCount){
+                        alert('Congratulations!! You\'ve won!');
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
+
+function draw(){
+    //clear each instance of the canvas so a new circle can be drawn
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    drawScore();
+    drawBricks();
+    drawBall();
+    drawPaddle();
+    collisionDetection;
+    //Calculate collision detections
+    //left and right walls
+    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    //top walls
+    if(y + dy < ballRadius){
+        dy = -dy;
+    }
+    else if(y + dy > canvas.height - ballRadius){
+        //detect paddle hits
+        if(x > paddleX && x < paddleX + paddleWidth){
+            dy = -dy;
+        }
+        //if no paddle hit, body of canvas is hit ==> game over
+        else {
+            alert('GAME OVER!!');
+            document.location.reload();
+        }
+    }
+    //bottom wall
+    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius){
+        dy = -dy;
+    }
+    //make paddle move
+    if(rightPressed && paddleX < canvas.width - paddleWidth){
+        paddleX += 7;
+    }
+    else if (leftPressed && paddleX > 0){
+        paddleX -= 7;
+    }
+    //maiking the ball move
+    x += dx; //update x movement every frame
+    y += dy; //update y movement every frame
+}
+
+//Create an infinite loop that creates the ball
+//paints the ball on the canvas every 10 milliseconds.
+setInterval(draw, 10)
